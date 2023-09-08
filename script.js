aa1461c0aea1f58bbfec7ed54c201f172da55663
bernazyzz <zebernardomoniz@gmail.com>
2023-09-08 23:21:02 +0100

    save and reset game

--- script.js
+++ script.js
@@ -91,6 +91,64 @@ function updateScorePerSecond() {
     document.getElementById('scorepersecond').innerHTML = scorePerSecond
 }
 
+function loadGame() {
+    var savedGame = JSON.parse(localStorage.getItem('gameSave'))
+    if (typeof savedGame.score !== 'undefined') score = savedGame.score
+    if (typeof savedGame.clickingPower !== 'undefined') clickingPower = savedGame.clickingPower
+    if (typeof savedGame.penCost !== 'undefined') penCost = savedGame.penCost
+    if (typeof savedGame.pens !== 'undefined') pens = savedGame.pens
+    if (typeof savedGame.scissorCost !== 'undefined') scissorCost = savedGame.scissorCost
+    if (typeof savedGame.scissors !== 'undefined') scissors = savedGame.scissors
+    if (typeof savedGame.hammerCost !== 'undefined') hammerCost = savedGame.hammerCost
+    if (typeof savedGame.hammers !== 'undefined') hammers = savedGame.hammers
+    if (typeof savedGame.chainsawCost !== 'undefined') chainsawCost = savedGame.chainsawCost
+    if (typeof savedGame.chainsaws !== 'undefined') chainsaws = savedGame.chainsaws
+    if (typeof savedGame.thorhammerCost !== 'undefined') thorhammerCost = savedGame.thorhammerCost
+    if (typeof savedGame.thorhammers !== 'undefined') thorhammers = savedGame.thorhammers
+}
+
+function saveGame() {
+    var gameSave = {
+        score: score,
+        clickingPower: clickingPower,
+        penCost: penCost,
+        pens: pens, 
+        scissorCost: scissorCost,
+        scissors: scissors,
+        hammerCost: hammerCost,
+        hammers: hammers,
+        chainsawCost: chainsawCost,
+        chainsaws: chainsaws,  
+        thorhammerCost: thorhammerCost, 
+        thorhammers: thorhammers 
+    }
+    localStorage.setItem('gameSave', JSON.stringify(gameSave))
+}
+
+function resetGame() {
+    if (confirm('Are you sure you want to reset your game?')) {
+        var gameSave = {}
+        localStorage.setItem('gameSave', JSON.stringify(gameSave))
+        location.reload()
+    }
+}
+
+window.onload = function() {
+    loadGame()
+    updateScorePerSecond()
+    document.getElementById('score').innerHTML = score
+    document.getElementById('pencost').innerHTML = penCost
+    document.getElementById('pens').innerHTML = pens
+    document.getElementById('scissorcost').innerHTML = scissorCost
+    document.getElementById('scissors').innerHTML = scissors
+    document.getElementById('hammercost').innerHTML = hammerCost
+    document.getElementById('hammers').innerHTML = hammers
+    document.getElementById('chainsawcost').innerHTML = chainsawCost
+    document.getElementById('chainsaws').innerHTML = chainsaws
+    document.getElementById('thorhammercost').innerHTML = thorhammerCost
+    document.getElementById('thorhammers').innerHTML = thorhammers
+}
+
 setInterval (function() {
     score += pens
     score += scissors * 3
@@ -102,3 +160,14 @@ setInterval (function() {
 
     document.title = score + " box - Box Clicker"
 }, 1000) // 1000ms = 1 second 
+
+setInterval (function() {
+    saveGame()
+}, 30000) // 30000ms = 30 seconds
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.which == 83) { // ctrl + s
+        event.preventDefault()
+        saveGame()
+    }
+}, false)
