c24c27267f43f167628ad94f1614f5b796edb540
bernazyzz <zebernardomoniz@gmail.com>
2023-09-10 00:22:01 +0100

    adjusted

--- script.js
+++ script.js
@@ -1,165 +1,130 @@
-var score = 0
-var clickingPower = 1
+var game = {
+    score: 0,
+    totalScore: 0,
+    clickValue: 1,
+    version: 0.000,
 
-var penCost = 15 
-var pens = 0
+    addToScore: function(amount) {
+        this.score += amount
+        this.totalScore += amount
+        display.updateScore()
+    }, 
 
-var scissorCost = 100
-var scissors = 0
-
-var hammerCost = 500
-var hammers = 0
-
-var chainsawCost = 1500
-var chainsaws = 0 
-
-var thorhammerCost = 10000
-var thorhammers = 0
-
-function addToScore(amount) {
-    score = score + amount
-    document.getElementById('score').innerHTML = score
-}
-
-function buyPen() {
-    if (score >= penCost) {
-        score = score - penCost
-        pens++
-        penCost = Math.round(penCost * 1.15)
-
-        document.getElementById('score').innerHTML = score
-        document.getElementById('pencost').innerHTML = penCost
-        document.getElementById('pens').innerHTML = pens
-        updateScorePerSecond()
+    getScorePerSecond: function() {
+        var scorePerSecond = 0
+        for (i = 0; i < building.name.length; i++) {
+            scorePerSecond += building.income[i] * building.count[i]
+        }
+        return scorePerSecond
     }
 }
 
-function buyScissor() {
-    if (score >= scissorCost) {
-        score = score - scissorCost
-        scissors++
-        scissorCost = Math.round(scissorCost * 1.15)
+var building = {
+    name: [
+        'Pen',
+        'Scissor',
+        'Hammer',
+        'Chainsaw',
+        'Thor Hammer'
+    ],
+    image: [
+        'pen.png',
+        'scissor.png',
+        'hammer.webp',
+        'chainsaw.webp',
+        'thor-hammer.png'
+    ],
+    count: [0, 0, 0, 0, 0],
+    income: [
+        1,
+        3,
+        5,
+        10,
+        50
+    ],
+    cost: [
+        15,
+        100,
+        500,
+        1500,
+        10000
+    ],
 
-        document.getElementById('score').innerHTML = score
-        document.getElementById('scissorcost').innerHTML = scissorCost
-        document.getElementById('scissors').innerHTML = scissors
-        updateScorePerSecond()
+    purchase: function(index) {
+        if (game.score >= this.cost[index]){
+            game.score -= this.cost[index]
+            this.count[index]++
+            this.cost[index]  = Math.ceil(this.cost[index] * 1.15)
+            display.updateScore()
+            display.updateShop()
+        }
     }
 }
+ 
+var display = {
+    updateScore: function() {
+        document.getElementById('score').innerHTML = game.score
+        document.getElementById('scorepersecond').innerHTML = game.getScorePerSecond()
+        document.title = game.score + ' box - Box Clicker'
+    },
 
-function buyHammer() {
-    if (score >= hammerCost) {
-        score = score - hammerCost
-        hammers++
-        hammerCost = Math.round(hammerCost * 1.15)
-
-        document.getElementById('score').innerHTML = score
-        document.getElementById('hammercost').innerHTML = hammerCost
-        document.getElementById('hammers').innerHTML = hammers
-        updateScorePerSecond()
+    updateShop: function() {
+        document.getElementById('shopContainer').innerHTML = ''
+        for (i = 0; i < building.name.length; i++) {
+            document.getElementById('shopContainer').innerHTML += '<table class="shopButton unselectable" onclick="building.purchase('+i+')"><tr><td class="image unselectable"><img src="images/'+building.image[i]+'" alt="pen"></td><td class="nameAndCost unselectable"><p>'+building.name[i]+'</p><p><span>'+building.cost[i]+'</span> box</p></td><td class="amount unselectable"><span>'+building.count[i]+'</span></td></tr></table>'
+        }
     }
-}
-
-function buyChainsaw() {
-    if (score >= chainsawCost) {
-        score = score - chainsawCost
-        chainsaws++
-        chainsawCost = Math.round(chainsawCost * 1.15)
-
-        document.getElementById('score').innerHTML = score
-        document.getElementById('chainsawcost').innerHTML = chainsawCost
-        document.getElementById('chainsaws').innerHTML = chainsaws
-        updateScorePerSecond()
-    }
-}
-
-function buyThorHammer() {
-    if (score >= thorhammerCost) {
-        score = score - thorhammerCost
-        thorhammers++
-        thorhammerCost = Math.round(thorhammerCost * 1.15)
-
-        document.getElementById('score').innerHTML = score
-        document.getElementById('thorhammercost').innerHTML = thorhammerCost
-        document.getElementById('thorhammers').innerHTML = thorhammers
-        updateScorePerSecond()
-    }
-}
-
-function updateScorePerSecond() {
-    scorePerSecond = pens + scissors * 3 + hammers * 5 + chainsaws * 10 + thorhammers * 50
-    document.getElementById('scorepersecond').innerHTML = scorePerSecond
-}
-
-function loadGame() {
-    var savedGame = JSON.parse(localStorage.getItem('gameSave'))
-    if (typeof savedGame.score !== 'undefined') score = savedGame.score
-    if (typeof savedGame.clickingPower !== 'undefined') clickingPower = savedGame.clickingPower
-    if (typeof savedGame.penCost !== 'undefined') penCost = savedGame.penCost
-    if (typeof savedGame.pens !== 'undefined') pens = savedGame.pens
-    if (typeof savedGame.scissorCost !== 'undefined') scissorCost = savedGame.scissorCost
-    if (typeof savedGame.scissors !== 'undefined') scissors = savedGame.scissors
-    if (typeof savedGame.hammerCost !== 'undefined') hammerCost = savedGame.hammerCost
-    if (typeof savedGame.hammers !== 'undefined') hammers = savedGame.hammers
-    if (typeof savedGame.chainsawCost !== 'undefined') chainsawCost = savedGame.chainsawCost
-    if (typeof savedGame.chainsaws !== 'undefined') chainsaws = savedGame.chainsaws
-    if (typeof savedGame.thorhammerCost !== 'undefined') thorhammerCost = savedGame.thorhammerCost
-    if (typeof savedGame.thorhammers !== 'undefined') thorhammers = savedGame.thorhammers
 }
 
 function saveGame() {
     var gameSave = {
-        score: score,
-        clickingPower: clickingPower,
-        penCost: penCost,
-        pens: pens, 
-        scissorCost: scissorCost,
-        scissors: scissors,
-        hammerCost: hammerCost,
-        hammers: hammers,
-        chainsawCost: chainsawCost,
-        chainsaws: chainsaws,  
-        thorhammerCost: thorhammerCost, 
-        thorhammers: thorhammers 
+        score: game.score,
+        totalScore: game.totalScore,
+        totalClicks: game.totalClicks,
+        version: game.version,
+        buildingCount: building.count,
+        buildingIncome: building.income,
+        buildingCost: building.cost
     }
     localStorage.setItem('gameSave', JSON.stringify(gameSave))
 }
 
-function resetGame() {
-    if (confirm('Are you sure you want to reset your game?')) {
-        var gameSave = {}
-        localStorage.setItem('gameSave', JSON.stringify(gameSave))
-        location.reload()
+function loadGame() {
+    var savedGame = JSON.parse(localStorage.getItem('gameSave'))
+    if (localStorage.getItem('gameSave') !== null) {
+        if (typeof savedGame.score !== 'undefined') game.score = savedGame.score
+        if (typeof savedGame.totalScore !== 'undefined') game.totalScore = savedGame.totalScore
+        if (typeof savedGame.clickValue !== 'undefined') game.clickValue = savedGame.clickValue
+        if (typeof savedGame.totalClicks !== 'undefined') game.totalClicks = savedGame.totalClicks
+        if (typeof savedGame.buildingCount !== 'undefined') {
+            for (i = 0; i < savedGame.buildingCount.length; i++) {
+                building.count[i] = savedGame.buildingCount[i]
+            }
+        }
+        if (typeof savedGame.buildingIncome !== 'undefined') {
+            for (i = 0; i < savedGame.buildingIncome.length; i++) {
+                building.income[i] = savedGame.buildingIncome[i]
+            }
+        }
+        if (typeof savedGame.buildingCost !== 'undefined') {
+            for (i = 0; i < savedGame.buildingCost.length; i++) {
+                building.cost[i] = savedGame.buildingCost[i]
+            }
+        }
     }
 }
 
 window.onload = function() {
     loadGame()
-    updateScorePerSecond()
-    document.getElementById('score').innerHTML = score
-    document.getElementById('pencost').innerHTML = penCost
-    document.getElementById('pens').innerHTML = pens
-    document.getElementById('scissorcost').innerHTML = scissorCost
-    document.getElementById('scissors').innerHTML = scissors
-    document.getElementById('hammercost').innerHTML = hammerCost
-    document.getElementById('hammers').innerHTML = hammers
-    document.getElementById('chainsawcost').innerHTML = chainsawCost
-    document.getElementById('chainsaws').innerHTML = chainsaws
-    document.getElementById('thorhammercost').innerHTML = thorhammerCost
-    document.getElementById('thorhammers').innerHTML = thorhammers
+    display.updateScore()
+    display.updateShop()
 }
 
-setInterval (function() {
-    score += pens
-    score += scissors * 3
-    score += hammers * 5
-    score += chainsaws * 10
-    score += thorhammers * 50
-
-    document.getElementById('score').innerHTML = score
-
-    document.title = score + " box - Box Clicker"
-}, 1000) // 1000ms = 1 second 
+setInterval(function() {
+    game.score += game.getScorePerSecond()
+    game.totalScore += game.getScorePerSecond()
+    display.updateScore()
+}, 1000) // 1000ms = 1 second
 
 setInterval (function() {
     saveGame()
